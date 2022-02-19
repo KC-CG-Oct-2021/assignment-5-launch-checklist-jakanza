c5137a4cdbd7d511aa5947e12e0b89347517267e
jakanza <67389200+jakanza@users.noreply.github.com>
2022-02-19 10:47:47 -0600

    added part 1

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,43 +1,19 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
+// Updates the missionTarget div - does not need to return anything
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+   // Here is the HTML formatting for our missionTarget div.
+   missiontarget = document.getElementById("missionTarget");
+   missiontarget.innerHTML = `
+    <h2>MissionDestination</h2>
+    <ol>
+        <li>Name: ${name}</li>
+        <li>Diameter: ${diameter} </li>
+        <li>Star: ${star}</li>
+        <li>Distance from Earth: ${distance}</li>
+        <li>Number of Moons: ${moons}</li>
+    </ol>
+    <img src=${imageUrl}>
+   `
 }
-
-function validateInput(testInput) {
-   
-}
-
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
-}
-
-async function myFetch() {
-    let planetsReturned;
-
-    planetsReturned = await fetch().then( function(response) {
-        });
-
-    return planetsReturned;
-}
-
-function pickPlanet(planets) {
-}
-
-module.exports.addDestinationInfo = addDestinationInfo;
-module.exports.validateInput = validateInput;
-module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
-module.exports.myFetch = myFetch;
